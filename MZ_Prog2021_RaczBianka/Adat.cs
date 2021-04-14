d09e858ec17bc5d6cada9296f14303cfabde77cf
RáczBianka <bianka.racz@gmail.com>
2021-04-14 17:56:19 +0200

    1. feladat kész

---
+++ MZ_Prog2021_RaczBianka/Adat.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MZ_Prog2021_RaczBianka
+{
+    class Adat
+    {
+        // név;első;utolsó;súly;magasság
+        public string Nev;
+        public string Elso;
+        public string Utolso;
+        public int Suly;
+        public int Magassag;
+
+        public Adat(string sor)
+        {
+            string[] s = sor.Split(';');
+            this.Nev = s[0];
+            this.Elso = s[1];
+            this.Utolso = s[2];
+            this.Suly = int.Parse(s[3]);
+            this.Magassag = int.Parse(s[4]);
+        }
+
+
+
+
+    }
+}
---
+++ MZ_Prog2021_RaczBianka/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MZ_Prog2021_RaczBianka
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // fájlbeolvasás
+            string[] sorok = File.ReadAllLines("balkezesek.csv");
+            List<Adat> adatok = new List<Adat>();
+            foreach (string sor in sorok.Skip(1))
+            {
+                adatok.Add(new Adat(sor));
+            }
+
+            // 1. feladat
+            int N = adatok.Count;
+            Console.WriteLine($"Hány versenyzőről van adatunk? Válasz: {N}");
+            
+            
+
+
+
+            Console.ReadLine();
+        }
+    }
+}
