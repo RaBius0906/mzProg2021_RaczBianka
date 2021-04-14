eb95418f3d12f427b52c7c1cd3bb2524bc843d27
RáczBianka <bianka.racz@gmail.com>
2021-04-14 18:39:29 +0200

    köztes állapot, nem sorrendben csinálom a feladatokat

--- MZ_Prog2021_RaczBianka/Adat.cs
+++ MZ_Prog2021_RaczBianka/Adat.cs
@@ -10,22 +10,23 @@ namespace MZ_Prog2021_RaczBianka
     {
         // név;első;utolsó;súly;magasság
         public string Nev;
-        public string Elso;
-        public string Utolso;
+        public int Elso;
+        public int Utolso;
         public int Suly;
         public int Magassag;
+
 
         public Adat(string sor)
         {
             string[] s = sor.Split(';');
             this.Nev = s[0];
-            this.Elso = s[1];
-            this.Utolso = s[2];
+            this.Elso = int.Parse(s[1].Substring(0,4));            
+            this.Utolso = int.Parse(s[2].Substring(0, 4));           
             this.Suly = int.Parse(s[3]);
             this.Magassag = int.Parse(s[4]);
         }
 
-
+        public string getNev() { return Nev; }
 
 
     }
--- MZ_Prog2021_RaczBianka/Program.cs
+++ MZ_Prog2021_RaczBianka/Program.cs
@@ -9,6 +9,7 @@ namespace MZ_Prog2021_RaczBianka
 {
     class Program
     {
+        public static int i;
         static void Main(string[] args)
         {
             // fájlbeolvasás
@@ -22,8 +23,45 @@ namespace MZ_Prog2021_RaczBianka
             // 1. feladat
             int N = adatok.Count;
             Console.WriteLine($"Hány versenyzőről van adatunk? Válasz: {N}");
-            
-            
+
+            // 2. feladat - Kik azok a versenyzők, akik 1980-ban léptek először pályára?
+            List<string> ElsoPalya1980 = new List<string>();
+            foreach (Adat adat in adatok)
+            {
+                if (adat.Elso == 1980)
+                {
+                    ElsoPalya1980.Add(adat.getNev());
+                }
+            }
+
+            // 3. feladat - Adott név szerepel-e a 2. feladat listájában, ha igen irja ki amagasságát, ha nem "hibás adat!" üzenet jelenjen meg.
+            // magasság adat inch-ben, 1 tizedes jegyre kerekítve
+            // commit
+
+            // 4. feladat - Válasszunk egy tetszőleges évszámot és irassunk ki mindent azokról a versenyzőkről akik ebben az évben léptek először a pályára.
+
+            // 5. feladat - Melyik a legkorábbi év amikor pályára léptek? V:1969
+            int legkorabbiKezdesIndex = 0;
+            for (i = 1; i < N; i++)
+            {
+                if (adatok[i].Elso < adatok[legkorabbiKezdesIndex].Elso)
+                {
+                    legkorabbiKezdesIndex = i;
+                }
+            }
+            Console.WriteLine($"Melyik a legkorábbi év amikor pályára léptek? Válasz: {adatok[legkorabbiKezdesIndex].Elso}");
+
+            // 6. feladat - Minden játékos 2000 előtt lépett utoljára pályára? V:igen
+            i = 0;
+            while (i < N && adatok[i].Utolso < 2000) { i++; }
+            bool mindenki2000elott = i >= N;
+            string valasz = mindenki2000elott ? "igen" : "nem";
+            Console.WriteLine($"Minden játékos 2000 előtt lépett utoljára pályára? Válasz: {valasz}");
+
+            // 7. feladat - Hány olyan játékos van akinek a nevében szerepel a John, sorold fel ezeket a neveket.
+
+            // 8. feladat - 
+
 
 
 
